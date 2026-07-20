547294d9abc003c2bc330a20cad88cc425df2715
suresh <suresh.ragam@icloud.com>
2026-07-20 14:58:55 +0530

    Initial Android project

---
+++ app/src/main/java/com/sureshragam/smarthomecamera/ui/MainActivity.kt
@@ -0,0 +1,46 @@
+package com.sureshragam.smarthomecamera.ui
+
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.activity.enableEdgeToEdge
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.Scaffold
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.tooling.preview.Preview
+
+class MainActivity : androidx.activity.ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+        setContent {
+            _root_ide_package_.com.sureshragam.smarthomecamera.ui.theme.SmartHomeCameraTheme {
+                Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
+                    Greeting(
+                        name = "Android",
+                        modifier = Modifier.padding(innerPadding)
+                    )
+                }
+            }
+        }
+    }
+}
+
+@Composable
+fun Greeting(name: String, modifier: Modifier = Modifier) {
+    Text(
+        text = "Hello $name!",
+        modifier = modifier
+    )
+}
+
+@Preview(showBackground = true)
+@Composable
+fun GreetingPreview() {
+    _root_ide_package_.com.sureshragam.smarthomecamera.ui.theme.SmartHomeCameraTheme {
+        Greeting("Android")
+    }
+}
